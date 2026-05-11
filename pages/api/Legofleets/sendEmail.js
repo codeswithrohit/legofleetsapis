bf8ab630567dc06da3ff5c3a05547743f09e6425
codeswithrohit <110283451+codeswithrohit@users.noreply.github.com>
2026-05-11 14:29:07 +0530

    Update sendEmail.js
    dwfer

--- pages/api/Legofleets/sendEmail.js
+++ pages/api/Legofleets/sendEmail.js
@@ -1,6 +1,13 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
